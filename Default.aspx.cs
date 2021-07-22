007328ad93b1ae02967170938c7b9ff45dae09f8
erorr404 <62029248+erorr404@users.noreply.github.com>
2021-07-22 19:37:41 +0530

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using Domain;
+
+namespace ARIS
+{
+    public partial class Default1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text.Trim()=="" || txtPassword.Text.Trim()=="")
+            {
+                this.lblError.Text = "Please enter username and password";
+            }
+            else
+            {
+                this.lblError.Text = "";
+
+                var username = txtUsername.Text.Trim();
+                var password = txtPassword.Text.Trim();
+
+                var obj = new Users
+                {
+                    Username = username,
+                    Password = password
+                };
+
+                var user = UsersDAL.CheckLogin(obj);
+
+                if(user != null)
+                {
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    lblError.Text = "Invalid Username or Password";
+                }
+            }
+        }
+    }
+}
---
+++ NewHome.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="NewHome.aspx.cs" Inherits="ARIS.NewHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
